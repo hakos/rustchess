6bdb3778a65463682a50029d2c911aad1bd22f57
Håkon Sandsmark <hsandsma@cisco.com>
2018-12-17 23:41:54 +0100

    Basic implementation of UCI protocol

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,48 @@
 mod board;
 
 fn main() {
-    use std::env;
     use std::io;
     use std::io::prelude::*;
 
-    println!("Welcome to Rust Chess!");
-
-    let mut board = if let Some(fen) = env::args().nth(1) {
-        board::Board::from_fen(&fen)
-    } else {
-        board::Board::initial_position()
-    };
-
-    let print_state = |board: &board::Board| {
-        println!("FEN: {}", board.as_fen());
-        board.print();
-        if board.is_check_mated() {
-            println!("Check mate! {:?} wins", board.turn().other());
-            std::process::exit(0);
-        };
-        if board.is_stale_mate() {
-            println!("Stale mate! It's a draw");
-            std::process::exit(0);
-        };
-        if board.is_checked() {
-            print!("Check! ");
-        };
-        println!("{:?}'s turn", board.turn());
-    };
-
-    print_state(&board);
+    let mut board = board::Board::initial_position();
     let stdin = io::stdin();
-    loop {
-    //for line in stdin.lock().lines() {
-        //if board.turn() == board::Color::White {
-            //let user_move = line.unwrap();
-            //if board.make_move(&user_move) {
-            print_state(&board);
-            //println!("Computer is thinking...");
-            let computer_move = board.negamax_iterative_deepening(std::time::Duration::new(20, 0));
-            println!("Computer move {} with evaluation {}", computer_move.1, computer_move.0);
-            assert!(board.make_move(&format!("{}", computer_move.1)));
-        //}
+
+    for line in stdin.lock().lines() {
+        let command = line.unwrap();
+        let mut parts = command.split_whitespace().into_iter();
+        match parts.next() {
+            Some("uci") => {
+                println!("id name RustChess");
+                println!("id author Håkon Sandsmark");
+                println!("uciok");
+            },
+            Some("isready") => println!("readyok"),
+            Some("position") => {
+                if parts.next() == Some("startpos") && parts.next() == Some("moves") {
+                    board = board::Board::initial_position();
+                    for m in parts {
+                        assert!(board.make_move(m));
+                    }
+                }
+            },
+            Some("go") => {
+                let mut time_left = std::time::Duration::from_secs(10);
+                while let Some(part) = parts.next() {
+                    if (part == "wtime" && board.turn() == board::Color::White)
+                    || (part == "btime" && board.turn() == board::Color::Black) {
+                        let millis = parts.next().expect("time").parse::<u64>().unwrap();
+                        time_left = std::time::Duration::from_millis(millis);
+                    }
+                }
+                let time_budget = time_left / 10;
+                println!("info string search budget {:?}", time_budget);
+                let computer_move = board.negamax_iterative_deepening(time_budget);
+                assert!(board.make_move(&format!("{}", computer_move.1)));
+                println!("info string moved {} with evaluation {}",
+                    computer_move.1, computer_move.0 as f32 / 10.0);
+                println!("bestmove {}", computer_move.1);
+            }
+            _ => (),
+        };
     }
-    println!("Bye");
 }
